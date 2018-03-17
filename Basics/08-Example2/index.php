e0a9b6af8f035c55540c4cd269879970d886bd4e
Gaël Damour <gael.damour@gmail.com>
2018-03-17 17:11:01 +0100

    Chapter 8 - Example 2 Completed

---
+++ Basics/08-Example2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Test formulaire</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    </head>
+    <body>
+        <?php
+
+        require 'Form.php';
+        require 'BootstrapForm.php';
+
+        $form = new BootstrapForm($_POST);
+
+        ?>
+
+        <form action="#" method="post">
+            <?php
+
+            echo $form->input('username');
+            echo $form->input('password');
+            echo $form->submit();
+            ?>
+
+        </form>
+
+    </body>
+</html>
+
+
